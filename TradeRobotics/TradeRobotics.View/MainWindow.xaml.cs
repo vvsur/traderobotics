bc044dcc3c6f37fd1c1e57d63b3354d3f97117b0
iron_stranger <iron_stranger@mail.ru>
2010-10-26 01:45:37 +0000

    Visifire chart added

--- TradeRobotics/TradeRobotics.View/MainWindow.xaml.cs
+++ TradeRobotics/TradeRobotics.View/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Visifire.Charts;
+using TradeRobotics.Model;
 
 namespace TradeRobotics.View
 {
@@ -27,7 +29,44 @@ namespace TradeRobotics.View
         private void LoadBarsButton_Click(object sender, RoutedEventArgs e)
         {
             TradeRobotics.DataProviders.History.HistoryDataProvider provider = new DataProviders.History.HistoryDataProvider();
-            provider.LoadBars("Sber03", 5);
+            var barCollection = provider.LoadBars("Sber", 5);
+            LoadPriceChart(barCollection);
         }
+
+        private void LoadPriceChart(BarCollection barCollection)
+        {
+
+            List<Bar> bars = barCollection.Bars;
+            DataSeries ds = new DataSeries();
+            ds.RenderAs = RenderAs.CandleStick;
+            ds.MarkerEnabled = true;
+            ds.MovingMarkerEnabled = true;
+            ds.LightingEnabled = true;
+            //ds.LineThickness = 1.5;
+            ds.XValueType = ChartValueTypes.DateTime;
+            ds.LegendText = string.Concat(barCollection.Symbol, " ", barCollection.Period);
+            ds.PriceUpColor = new SolidColorBrush(Colors.Green);
+            ds.PriceDownColor = new SolidColorBrush(Colors.Red);
+            
+            //PriceChart.AxesY[0].AxisMinimum = bars.Min(bar => bar.Low);
+            //PriceChart.AxesY[0].AxisMaximum = bars.Max(bar => bar.High);
+            PriceChart.AxesY[0].StartFromZero = false;
+            PriceChart.AxesY[0].ViewportRangeEnabled = true;
+
+            // Add points
+            foreach (Bar bar in barCollection.Bars)
+            {
+                ds.DataPoints.Add(new DataPoint
+                {
+                    XValue = bar.Time, // a DateTime value
+                    YValues = new double[] { bar.Open, bar.Close, bar.High, bar.Low } // a double value
+                });
+
+            }
+            
+            PriceChart.Series.Add(ds);
+
+        }
+
     }
 }
