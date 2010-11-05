16f8e470386d072308a9f0649cc0549a246d301e
iron_stranger@mail.ru <iron_stranger@mail.ru@54ebe25b-7941-9c80-b214-f932ebca0b8c>
2010-11-05 21:36:42 +0000

    Test and history DataProvider moved to one assembly

---
+++ trunk/TradeRobotics/TradeRobotics.DataProviders/HistoryDataProvider.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TradeRobotics.Model;
+using System.IO;
+using TradeRobotics.TradeLibrary;
+using System.Text.RegularExpressions;
+
+namespace TradeRobotics.DataProviders
+{
+    /// <summary>
+    /// Provider historical data from file
+    /// </summary>
+    public class HistoryDataProvider:IDataProvider
+    {
+        /// <summary>
+        /// Data series
+        /// </summary>
+        public StockDataSeries DataSeries {get;set;}
+
+        #region Load data from file
+        /// <summary>
+        /// 
+        /// </summary>
+        public Tuple<string, int, bool> GetDataFileInfo(string fileName)
+        {
+            Regex regex = new Regex(@"(?<name>\w+)_(?<periodName>[m,d])(?<periodValue>\d+)(?<quotes>_quotes)*\.csv$", RegexOptions.IgnoreCase);
+
+            Match match = regex.Match(fileName);
+            if(!match.Success)
+                return new Tuple<string, int, bool>(null,0,false);
+
+            string name = match.Groups["name"].Value;
+            string periodName = match.Groups["periodName"].Value;
+            int periodValue = Convert.ToInt32(match.Groups["periodValue"].Value);
+            if(periodName == "H") 
+                periodValue = 60*periodValue;
+            if(periodName == "D")
+                periodValue = 60*24*periodValue;
+            bool isQuotes = (match.Groups["quotes"].Value == "_quotes");
+
+            return new Tuple<string,int,bool>(name, periodValue, isQuotes);
+    
+        }
+        
+        //public const string historyFileName = @"{0}_M{1}.csv";
+        public const string quotesHistoryFileName = @"{0}_{1}_quotes.csv";
+        
+        /// <summary>
+        /// Get bars from file
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <param name="period"></param>
+//        public BarCollection LoadBars(string symbol, int period)
+        public StockDataSeries LoadBars(string filePath)
+        {
+            // Load from file
+            //string filePath = string.Format(historyFileName, symbol, period);
+            Tuple<string, int, bool> dataInfo = GetDataFileInfo(filePath);
+            filePath = string.Concat(DataContext.DataDirectory, filePath);
+
+            string[] lines = File.ReadAllLines(filePath);
+            int i = 0;
+            // Init quik bar
+            DataSeries = new StockDataSeries();
+            DataSeries.Symbol = dataInfo.Item1;
+            DataSeries.Period = dataInfo.Item2; ;
+            // Add bars
+            foreach (string line in lines)
+            {
+                // First line is a header, no parse
+                if (i++ == 0)
+                    continue;
+                Bar bar = ConverterHelper.LoadBar(line);
+
+                DataSeries.Bars.Add(bar);
+                DataSeries.Times.Add(bar.Time);
+                DataSeries.Open.Add(bar.Open);
+                DataSeries.Low.Add(bar.Low);
+                DataSeries.High.Add(bar.High);
+                DataSeries.Close.Add(bar.Close);
+                DataSeries.Volume.Add(bar.Volume);
+            }
+            
+            return DataSeries;
+        }
+
+        /// <summary>
+        /// Load quotes for one day
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public List<Quote> LoadQuotes(string symbol, DateTime date)
+        {
+            string fileName = string.Format(quotesHistoryFileName, symbol, date.ToString("yyyy.MM.dd"));
+            string filePath = string.Concat(DataContext.DataDirectory, fileName);
+            return LoadQuotesFromFile(filePath);
+
+        }
+        
+        /// <summary>
+        /// Load all quotes for symbol
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        public List<Quote> LoadQuotes(string symbol)
+        {
+            string searchPattern = string.Format(quotesHistoryFileName, symbol, "*");
+
+            string[] fileNames = Directory.GetFiles(DataContext.DataDirectory, searchPattern);
+            List<Quote> allQuotes = new List<Quote>();
+            foreach (string file in fileNames)
+            {
+                allQuotes.AddRange(LoadQuotesFromFile(file));
+            }
+            return allQuotes;
+        }
+        
+        /// <summary>
+        /// Get quotes from file
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        protected List<Quote> LoadQuotesFromFile(string filePath)
+        {
+            /*string filePath = string.Format(quotesHistoryFileName, symbol);
+            filePath = string.Concat(DataContext.DataDirectory, filePath);*/
+            
+            string[] lines = File.ReadAllLines(filePath);
+            int i = 0;
+            List<Quote> quotes = new List<Quote>();
+            // Add bars
+            foreach (string line in lines)
+            {
+                // First line is a header, no parse
+                if (i++ == 0)
+                    continue;
+                Quote quote = ConverterHelper.LoadQuote(line);
+                quotes.Add(quote);
+
+            }
+            return quotes;
+
+        }
+        #endregion
+
+        /// <summary>
+        /// New history data
+        /// </summary>
+        public event EventHandler<TickEventArgs> Tick;
+    }
+}
---
+++ trunk/TradeRobotics/TradeRobotics.DataProviders/TestDataProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TradeRobotics.TradeLibrary;
+using TradeRobotics.Model;
+
+namespace TradeRobotics.DataProviders
+{
+    /// <summary>
+    /// Robot test data provider
+    /// </summary>
+    public class TestDataProvider:HistoryDataProvider, IDataProvider
+    {
+        /// <summary>
+        /// Testing robot
+        /// </summary>
+        public IRobot Robot
+        {
+            get
+            {
+                return robot;
+            }
+            set
+            {
+                    if (robot != null)
+                        robot.StateChanged -= OnRobotStateChanged;
+                    robot = value;
+                    if (robot != null)
+                        robot.StateChanged += OnRobotStateChanged;
+            }
+        }
+        private IRobot robot;
+
+        /// <summary>
+        /// Current timeline bar index
+        /// </summary>
+        public int BarIndex = 0;
+        
+        
+        /// <summary>
+        /// When robot came to idle state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void OnRobotStateChanged(object sender,  RobotStateChangedEventArgs e)
+        {
+            if (e.NewState == RobotState.Idle && BarIndex < DataSeries.Count)
+                Tick(this, new TickEventArgs(DataSeries, BarIndex++));
+        }
+
+        /// <summary>
+        /// Start new test
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="robot"></param>
+        public void BeginTest( IRobot robot)
+        {
+            // Connect robot to provider
+            robot.DataProvider = this;
+            this.Robot = robot;
+            BarIndex = 0;
+            TestContext.CurrentBarIndex = BarIndex;
+            TestContext.CurrentTime = DataSeries.Times[BarIndex];
+            // First tick
+            Tick(this, new TickEventArgs(DataSeries,BarIndex++));
+        }
+
+        /// <summary>
+        /// Stop test, disconnect from robot
+        /// </summary>
+        public void StopTest()
+        {
+            this.Robot = null;
+
+        }
+        /// <summary>
+        /// New data tick
+        /// </summary>
+        public new event EventHandler<TickEventArgs> Tick;
+    }
+}
