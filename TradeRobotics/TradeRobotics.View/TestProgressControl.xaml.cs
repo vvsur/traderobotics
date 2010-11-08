1a636b12931579c964ad46520e6b00bf6b5e5561
iron_stranger <iron_stranger@mail.ru>
2010-11-08 22:22:07 +0000

    Progress bar moved to separate control

---
+++ TradeRobotics/TradeRobotics.View/TestProgressControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TradeRobotics.View
+{
+    /// <summary>
+    /// Interaction logic for TestProgressControl.xaml
+    /// </summary>
+    public partial class TestProgressControl : UserControl
+    {
+        public TestProgressControl()
+        {
+            InitializeComponent();
+        }
+
+        public double Value
+        {
+            get
+            {
+                return ProgressBar1.Value;
+            }
+            set
+            {
+                ProgressBar1.Value = value;
+            }
+        }
+
+    }
+}
